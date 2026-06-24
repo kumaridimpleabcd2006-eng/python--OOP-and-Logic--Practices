5f1c6b1062bf3cbd5a402fbba0ff33ecbf4a9d78
Dimple Kumari <kumaridimpleabcd2006@gmail.com>
2026-06-24 21:40:01 +0530

    Add files via upload

---
+++ student_marks analyzer.py
@@ -0,0 +1,12 @@
+marks =[78,92,65,88,95]
+total = sum(marks)
+print(f"Total Marks: {total}")
+
+average = total/len(marks)
+print(f"Average Marks: {average}")
+
+highest = max(marks)
+print(f"Highest Marks: {highest}")
+
+lowest = min(marks)
+print(f"Lowest Marks: {lowest}")
